a5ee2f94ae6c3b6fad526c6088d13b85c8b51472
marcelo <paris.jm@gmail.com>
2017-08-10 20:39:56 -0400

    EJERCICIO PARA VERIFICAR NUMERO POSIITOV, PAR Y MULTIPLO DE 5

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -20,7 +20,63 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+    int C = 9;
+    if (C >=0){
+    System.out.println("El numero es positvo");
+    
+    }
+    else {
+    System.err.println("el numero es negativo");
+    
+    } 
+
+    
+    if (C%2==0) {
+    System.out.println("el numero es par");
+    
+   }
+    else {
+    System.out.println("el numero es impar");
     }
     
+    
+    if (C%5==0){
+    System.out.println("el numero es multiplo de 5");
+    }
+    
+    else {
+        System.out.println("el numero no es multiplo de 5");
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
 }
+
+
+
+
+
+
+
