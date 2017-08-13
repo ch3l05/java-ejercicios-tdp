e725f373f9ff49f0df0ed36bddd9b1a07855a116
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 20:27:16 -0300

    ejercicio09 branch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ejercicios sobre tecnicas de programación (TDP) en Java
 
-Los ejercicios estan separados en diferentes ramas del repositorio, la idea es que cada estudiante descargue el repositorio, elija uno de los ejercicios y lo resuelva.
+## EJERCICIO 09: Programa java que realice lo siguiente
+
+* Lea un nombre y muestre por pantalla: “Buenos dias nombre_introducido”.
+
+## SPOILER ALERT
+
+Posible solución [aquí](http://puntocomnoesunlenguaje.blogspot.com.es/2012/10/java-ejercicios-iniciales.html).
--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 09:
+ * Programa Java que lea un nombre y muestre por pantalla: “Buenos dias nombre_introducido”.
  */
 package cac.java.tdp;
 
@@ -15,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
